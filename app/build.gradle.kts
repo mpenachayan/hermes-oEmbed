83409bb7b78f4f35ad9b7c6b286c9878a0e5ec44
Martín Pena <1216950-mpenachayan@users.noreply.gitlab.com>
2019-11-01 12:28:56 +0100

    Initial Upload of base project

---
+++ app/build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(29)
+    buildToolsVersion = "29.0.2"
+    defaultConfig {
+        applicationId = "gal.mpena.hermes.twitter"
+        minSdkVersion(21)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    sourceSets["main"].java.srcDir("src/main/kotlin")
+    sourceSets["test"].java.srcDir("src/test/kotlin")
+    sourceSets["androidTest"].java.srcDir("src/androidTest/kotlin")
+}
+
+dependencies {
+
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(kotlin("stdlib-jdk8", KotlinCompilerVersion.VERSION))
+    
+    //Android X libraries
+    implementation ("androidx.appcompat:appcompat:1.1.0")
+    implementation ("androidx.core:core-ktx:1.1.0")
+    implementation ("androidx.constraintlayout:constraintlayout:1.1.3")
+    
+    //Material Design libraries
+    implementation ("com.google.android.material:material:1.1.0-beta01")
+
+    //Testing libraries
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test.ext:junit:1.1.0")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.2.0")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "hermes-twitter"
+rootProject.buildFileName = "build.gradle.kts"
+include(":app")
