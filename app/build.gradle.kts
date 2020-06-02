9f6323fc5db602bb2401347616a27a0b0ac43bd1
Martín Pena <1216950-mpenachayan@users.noreply.gitlab.com>
2020-06-02 07:15:00 +0000

    Update libraries to Andrpoid 11 Preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 }
 
 android {
-    compileSdkVersion(29)
-    buildToolsVersion = "29.0.3"
+    compileSdkVersion('android-R')
+    buildToolsVersion = "30.0.0 rc4"
     defaultConfig {
         applicationId = "gal.mpena.hermes.oembed"
         minSdkVersion(21)
-        targetSdkVersion(29)
+        targetSdkVersion('R')
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -32,15 +32,15 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     
     //Android X libraries
-    implementation ("androidx.appcompat:appcompat:1.1.0")
-    implementation ("androidx.core:core-ktx:1.2.0")
-    implementation ("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation ("androidx.appcompat:appcompat:1.2.0-rc01")
+    implementation ("androidx.core:core-ktx:1.3.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.0.0-beta6")
     
     //Material Design libraries
-    implementation ("com.google.android.material:material:1.1.0-rc02")
+    implementation ("com.google.android.material:material:1.2.0-beta01")
 
     //Testing libraries
     testImplementation("junit:junit:4.13")
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.2.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.2-rc01")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.3.0-rc01")
 }
